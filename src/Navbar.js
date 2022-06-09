a04bab6f4e4998fe634e1e6aecd12e054c5a7a55
Gabriel John Pil <gjspil@gmail.com>
2022-06-09 02:52:21 -0400

    Netflix clone app using React JS

---
+++ src/Navbar.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+import "./Navbar.css";
+
+function Navbar() {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && "navbar__black"}`}>
+      <img
+        className="nav__logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+        alt="Netflix Logo"
+      />
+      <img
+        className="nav__avatar"
+        src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/1bdc9a33850498.56ba69ac2ba5b.png"
+        alt="user"
+      />
+      <span className="profile__name">gjspil</span>
+    </div>
+  );
+}
+
+export default Navbar;
